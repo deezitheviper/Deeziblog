802a21b2ed79f2ec06af13011ffe42c34ba2ec56
deezitheviper <amuhdavid@gmail.com>
2022-10-13 08:52:55 +0100

    route update

--- api/server.js
+++ api/server.js
@@ -1,6 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import postRouter from './routes/Post.js';
+import userRouter from './routes/User.js';
+import authRouter from './routes/Auth.js';
+
+
 
 const app = express();
 
@@ -8,6 +13,7 @@ dotenv.config()
 
 app.use(express.json())
 
+//Database
 const connect = async  () => {
     const connection = await mongoose.connect(process.env.MONGODB)
     .catch(
@@ -19,7 +25,6 @@ const connect = async  () => {
 }
 
 
-
 mongoose.connection.on('connected', () => {
     console.log("Connected")
   }); 
@@ -27,6 +32,16 @@ mongoose.connection.on('connected', () => {
 mongoose.connection.on('disconnected', () => {
     console.log("Disconnected")
   });
+
+
+//Routes
+app.get('/', (req, res)=>{
+    res.send("Hello")
+})
+app.use('/api/users', userRouter)
+app.use('/api/auth', authRouter)
+app.use('/api/post', postRouter)
+
 
 app.listen(5000,() => {
     connect()
