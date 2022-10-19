f5011d3e6963f1db4bb9176e5b22443b362c315f
deezitheviper <amuhdavid@gmail.com>
2022-10-19 18:35:27 +0100

    update on sidebar

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,10 +1,12 @@
-import React,{useState, useEffect} from 'react';
+import React,{useState, useEffect, useContext} from 'react';
 import instance from '../config/axios.js';
-
+import avatar from '../assets/img/avatar.png';
+import { AuthContext } from '../context/authContext.js';
 
 
 const Sidebar = ({cat}) => {
     const [posts, setPosts] = useState()
+    const {currentUser} = useContext(AuthContext)
 
     useEffect(() => {
         const getCatPost = async () => {
@@ -22,10 +24,14 @@ const Sidebar = ({cat}) => {
             <div className='sidebar'>
             <h3>More from Deeziblog</h3>
             {posts?.map(post => (
- <div className='content'>
+ <div className='content' key={post.id}>
  <div className='l-content'>
-  <div className='profile'>
-    <img src='https://www.fillmurray.com/640/360' alt="account" />
+  <div className='profile'> 
+  {currentUser?.profilepic?
+               <img src='https://www.fillmurray.com/640/360' alt="account" />
+               :
+               <img src={avatar} alt="account" /> 
+  }
     <div className='info'>
         <span>{post.authur}</span>    
      </div>
@@ -35,7 +41,7 @@ const Sidebar = ({cat}) => {
 
   </div>
   <div className='r-content'>
-  <img src={`../public/upload/${post.img}`} alt=""/>
+  <img src={`../upload/${post?.img}`} alt=""/>
   </div>
   </div>
             ))}
