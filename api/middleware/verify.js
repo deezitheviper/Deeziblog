650a5383b6a48f358eff462ab257b0bc1a849701
deezitheviper <amuhdavid@gmail.com>
2023-01-06 19:03:59 +0100

    update on v2 controller with mysql

--- api/middleware/verify.js
+++ api/middleware/verify.js
@@ -17,7 +17,7 @@ export const verifyUser = (req, res, next) => {
         if(req.user.id == req.params.id || req.user.isAdmin){
             next()
         }else{
-            return next(createError(403, "You're not authenticated"))
+            return next(createError(403, "You're not authorized"))
         }
     })
 }
